c6be4daf1c23a6b71ecd54ed4313fa310dc1282f
peterkrauz <krausepeterm@gmail.com>
2020-10-30 10:26:00 -0300

    Import the raven text

---
+++ deps.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/msg.h>
+#include <sys/time.h>
+#include "word_entry.c"
---
+++ word_entry.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct WordEntry{
+  char word[];
+  int occurrrences;
+};
