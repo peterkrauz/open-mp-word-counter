59cc7e6d23c9412a0d1ebb4eaccebc34e10db7a8
peterkrauz <krausepeterm@gmail.com>
2020-10-30 14:59:15 -0300

    Single threaded version working

--- deps.h
+++ deps.h
@@ -7,4 +7,10 @@
 #include <sys/msg.h>
 #include <string.h>
 #include <sys/time.h>
+#include <ctype.h>
 #include "word_entry.c"
+
+char* toLower(char* s) {
+  for(char *p=s; *p; p++) *p=tolower(*p);
+  return s;
+}
